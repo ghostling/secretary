6ad3f2ae098956fe7b4a95168cf315a29f443179
Jiexi Cao <jiexicao@gmail.com>
2015-06-05 17:51:57 -0700

    Moved test data out.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from pytz import timezone
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 
+from testdata import TEST_RULES
+
 # Pull in configuration from system environment variables
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
@@ -18,44 +20,6 @@ USER_TIMEZONE = timezone('US/Pacific')
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 app = Flask(__name__)
-
-# Dummy test data.
-# TODO: Move.
-rules = {
-    "+14134062242": {
-        "condition": "always",
-        "response": {
-            "type": "text",
-            "data": "Hello"
-            },
-        "take_message": 1
-        },
-    "+19193608390": {
-        "condition": "time",
-        "busy_intervals": [
-            { "label": "work",
-                "start": "09:00:00",
-                "end": "17:00:00"},
-            { "label": "sleep",
-                "start": "24:00:00",
-                "end": "08:00:00"},
-            ],
-        "response": {
-            "type": "audio",
-            "data": "static/audio/recording.mp3"
-            },
-        "take_message": 0
-        },
-    "*": {
-        "condition": "always",
-        "response": {
-            "type": "audio",
-            "data": "static/audio/test.mp3"
-            },
-        "take_message": 0
-        },
-}
-
 
 @app.route("/", methods=["GET"])
 def main():
@@ -100,10 +64,10 @@ def get_rule_for_call(request):
     from_number = request.values.get("From", None)
     caller_rule = {}
     # Load the rule to execute for this caller.
-    if from_number in rules:
-        caller_rule = rules[from_number]
+    if from_number in TEST_RULES:
+        caller_rule = TEST_RULES[from_number]
     else:
-        caller_rule = rules["*"] # The everyone else rule.
+        caller_rule = TEST_RULES["*"] # The everyone else rule.
     return caller_rule
 
 def create_response(response_rules, resp):
