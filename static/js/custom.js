f5dd7c9dc683de1551b1d263b8ae78b1a7f443b1
Jiexi Cao <jiexicao@gmail.com>
2015-06-10 18:40:25 -0700

    JS successfully converts data into nice cute little json ready to fly

--- static/js/custom.js
+++ static/js/custom.js
@@ -26,29 +26,38 @@ function addNewBusyTimeRows() {
 
 function bindCreateAlwaysRule() {
     var redirect_url= document.location.href;
+
     $("#condition-always-form").submit(function (e) {
         e.preventDefault();
+        var always_rule = {"condition": "always"};
         var caller_number = $("#form-caller-number").val();
         var action = $("input[name='radio-always-action']:checked").attr("id");
 
         if (action == "form-always-forward") {
-            // Do stuff
+            always_rule.forward = 1;
         } else if (action == "form-always-play") {
             // What to play
             var play_type = $("#form-always-play-type :selected").attr("id");
             if (play_type == "form-always-play-text") {
                 var text_to_play = $("#form-always-play-data").val();
+                always_rule.response = { "data" : text_to_play, "type" : "text" };
             } else if (play_type == "form-always-play-audio") {
                 // TODO: Implement later.
+                console.log("Audio not currently supported.")
             }
 
             // Take message or not
             if ($("#form-always-take-message").is(":checked")) {
-                // Take Message
+                always_rule.take_message = 1;
             } else {
-                // No
+                always_rule.take_message = 0;
             }
         }
+
+        // Assigns rule to specified caller number.
+        complete_rule = {};
+        complete_rule[caller_number] = always_rule;
+        always_rule_json = JSON.stringify(complete_rule);
     });
 };
 
@@ -56,15 +65,24 @@ function bindCreateTimeRule() {
     var redirect_url= document.location.href;
     $("#condition-time-form").submit(function (e) {
         e.preventDefault();
+        var time_rule = {"condition": "time"};
         var caller_number = $("#form-caller-number").val();
 
         // Get busy times
         var busy_entries = $(".busy-time-entry");
         var num_busy_entries = busy_entries.length;
+        time_rule.busy_intervals = {};
         for (var i = 0; i < num_busy_entries; i++) {
             var start_time = $(busy_entries[i]).find(".start-time").val();
             var end_time = $(busy_entries[i]).find(".end-time").val();
             var label = $(busy_entries[i]).find(".label").val();
+            if (start_time != "" && end_time != "" && label != "") {
+                time_rule.busy_intervals[i] = {
+                    "start": start_time,
+                    "end": end_time,
+                    "label": label
+                };
+            }
         }
 
         // Get busy rules
@@ -73,13 +91,24 @@ function bindCreateTimeRule() {
             var play_type = $("#form-time-busy-play-type :selected").attr("id");
             if (play_type == "form-time-busy-play-text") {
                 var text_to_play = $("#form-time-busy-play-data").val();
+                time_rule.response = { "data" : text_to_play, "type" : "text" };
             } else if (play_type == "#form-time-busy-play-audio") {
                 // TODO: Implement later.
+                console.log("Audio not currently supported.")
             }
         }
+
+        // Take message or not
         if ($("#form-time-busy-take-message").is(":checked")) {
-            // Take message
+            time_rule.take_message = 1;
+        } else {
+            time_rule.take_message = 0;
         }
+
+        // Assigns rule to specified caller number.
+        complete_rule = {};
+        complete_rule[caller_number] = time_rule;
+        time_rule_json = JSON.stringify(complete_rule);
     });
 };
 
