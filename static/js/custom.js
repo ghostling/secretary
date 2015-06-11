7ff93d427cc298750237a11753a40d7e7c129a67
Jiexi Cao <jiexicao@gmail.com>
2015-06-10 18:02:22 -0700

    Cleaned up the form and js for the form

--- static/js/custom.js
+++ static/js/custom.js
@@ -24,71 +24,71 @@ function addNewBusyTimeRows() {
     });
 };
 
-// Prepares "Create Rule" modal for POST request.
-// Note: I'm going to pretend I didn't write this function. T_T
-function postCreateRuleForm() {
-    var save_id = "#save-rule-button";
-    //var form_id = "#create-rule-form";
-    //var modal_id = "#create-rule-modal";
-    var post_url= "/create-rule";
+function bindCreateAlwaysRule() {
     var redirect_url= document.location.href;
-
-    $(save_id).click(function(e) {
+    $("#condition-always-form").submit(function (e) {
+        e.preventDefault();
         var caller_number = $("#form-caller-number").val();
-        var condition = $("input[name='radio-condition']:checked").attr("id");
+        var action = $("input[name='radio-always-action']:checked").attr("id");
 
-        if (condition == "form-call-condition-always") {
-            var action = $("input[name='radio-always-action']:checked").attr("id");
-            if (action == "form-always-forward") {
-                // Do stuff
-            } else if (action == "form-always-play") {
-                // What to play
-                var play_type = $("#form-always-play-type :selected").attr("id");
-                if (play_type == "form-always-play-text") {
-                    var text_to_play = $("#form-always-play-data").val();
-                } else if (play_type == "form-always-play-audio") {
-                    // TODO: Implement later.
-                }
-
-                // Take message or not
-                if ($("#form-always-take-message").is(":checked")) {
-                    // Take Message
-                } else {
-                    // No
-                }
-            }
-        } else if (condition == "form-call-condition-time") {
-            // Get busy times
-            var busy_entries = $(".busy-time-entry");
-            var num_busy_entries = busy_entries.length;
-            for (var i = 0; i < num_busy_entries; i++) {
-                var start_time = $(busy_entries[i]).find(".start-time").val();
-                var end_time = $(busy_entries[i]).find(".end-time").val();
-                var label = $(busy_entries[i]).find(".label").val();
+        if (action == "form-always-forward") {
+            // Do stuff
+        } else if (action == "form-always-play") {
+            // What to play
+            var play_type = $("#form-always-play-type :selected").attr("id");
+            if (play_type == "form-always-play-text") {
+                var text_to_play = $("#form-always-play-data").val();
+            } else if (play_type == "form-always-play-audio") {
+                // TODO: Implement later.
             }
 
-            // Get busy rules
-            if ($("#form-time-busy-play").is(":checked")) {
-                // What to play
-                var play_type = $("#form-time-busy-play-type :selected").attr("id");
-                if (play_type == "form-time-busy-play-text") {
-                    var text_to_play = $("#form-time-busy-play-data").val();
-                } else if (play_type == "#form-time-busy-play-audio") {
-                    // TODO: Implement later.
-                }
-
-            }
-            if ($("#form-time-busy-take-message").is(":checked")) {
-                // Take message
+            // Take message or not
+            if ($("#form-always-take-message").is(":checked")) {
+                // Take Message
+            } else {
+                // No
             }
         }
     });
-}
+};
+
+function bindCreateTimeRule() {
+    var redirect_url= document.location.href;
+    $("#condition-time-form").submit(function (e) {
+        e.preventDefault();
+        var caller_number = $("#form-caller-number").val();
+
+        // Get busy times
+        var busy_entries = $(".busy-time-entry");
+        var num_busy_entries = busy_entries.length;
+        for (var i = 0; i < num_busy_entries; i++) {
+            var start_time = $(busy_entries[i]).find(".start-time").val();
+            var end_time = $(busy_entries[i]).find(".end-time").val();
+            var label = $(busy_entries[i]).find(".label").val();
+        }
+
+        // Get busy rules
+        if ($("#form-time-busy-play").is(":checked")) {
+            // What to play
+            var play_type = $("#form-time-busy-play-type :selected").attr("id");
+            if (play_type == "form-time-busy-play-text") {
+                var text_to_play = $("#form-time-busy-play-data").val();
+            } else if (play_type == "#form-time-busy-play-audio") {
+                // TODO: Implement later.
+            }
+        }
+        if ($("#form-time-busy-take-message").is(":checked")) {
+            // Take message
+        }
+    });
+};
 
 function main() {
     bindToggleToCreateRuleCondition();
     addNewBusyTimeRows();
-    postCreateRuleForm();
+    //postCreateRuleForm();
+    bindCreateAlwaysRule();
+    bindCreateTimeRule();
 };
 
 main();
